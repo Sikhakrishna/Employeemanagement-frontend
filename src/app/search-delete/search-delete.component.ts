5dd236a12e278b99d2281a869ed5e94ea2ed6147
Sikhakrishna <sikhakrishna2017@gmail.com>
2021-06-01 11:11:37 +0530

    front end commit

--- src/app/search-delete/search-delete.component.ts
+++ src/app/search-delete/search-delete.component.ts
@@ -8,20 +8,38 @@ import { UserRegistrationService } from '../user-registration.service';
 })
 export class SearchDeleteComponent implements OnInit {
 employees:any;
-employeeId:number;
+employeeId:any;
 // message:any;
   constructor(private service:UserRegistrationService) { }
-
+  
+  public getEmployee(employeeId){
+    let resp=this.service.getEmployeebyId(this.employeeId);
+    resp.subscribe((data)=>this.employees=data);
+  }
 public deleteEmployee(employeeId){
 let resp=this.service.deleteemployeebyId(employeeId);
 resp.subscribe((data)=>this.employees=data);
 // this.service.addEmployee(employee).subscribe((data)=>this.message=data);
 }
 // constructor(private service:UserRegistrationService) { }
-public getEmployee(){
-  let resp=this.service.getEmployeebyId(this.employeeId);
-  resp.subscribe((data)=>this.employees=data);
-}
+// public getEmployee(){
+//   let resp=this.service.getEmployeebyId(this.employeeId);
+//   resp.subscribe((data)=>this.employees=data);
+// }
+
+// update(){
+  
+// const employee=new User();
+// employee.employeeId=this.sampleForm.get('employeeId').value;
+// employee.name=this.sampleForm.get('name').value;
+// employee.emailId=this.sampleForm.get('emailId').value;
+// employee.designation=this.sampleForm.get('designation').value;
+// this.service.updateEmployee(employee).subscribe(res=>{
+//   if(res){
+//     alert("updated successfully");
+//   }
+// })
+// }
 
   ngOnInit(): void {
     let resp=this.service.getemployees();
